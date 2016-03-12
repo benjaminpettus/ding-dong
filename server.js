4a273ffe93537660ae417cb480aef895254c2624
benjaminpettus <benjaminpettus@gmail.com>
2016-03-12 11:21:17 -1000

    working on app routes

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
-// var jade = require('jade');
 var mailgun = require('mailgun-js')({apiKey: 'key-5cf681c09c48516a8e0b8b00a580969c', domain: 'sandbox08ea930e43c140d699bd4406076fd445.mailgun.org'});
 var filepath = path.join(__dirname, 'IMG_1534.JPG');
-
+var jade = require('jade');
 var app = express();
+var CONFIG = require('./config.json');
+
+app.set('view engine', 'jade');
+app.set('views', path.resolve(__dirname, 'views'));
 
 app.use(express.static(path.resolve(__dirname, 'public')));
 app.use(bodyParser.urlencoded({extended: true}));
@@ -14,14 +17,22 @@ app.use(bodyParser.json());
 
 
 app.get('/', function (req, res) {
-  res.send('Home Page');
+  res.render('index');
 });
 
 app.get('/admin', function (req, res) {
-  res.send('Admin Page');
+   var locals = {
+    name:     CONFIG.name,
+    phone:    CONFIG.phone,
+    carrier:  CONFIG.carrier
+  };
+  res.render('admin', locals);
 });
 
 app.post('/', function (req, res) {
+ 
+
+  console.log(guest);
   res.render('admin');
 });
 
@@ -33,7 +44,6 @@ app.post('/message', function (req, res) {
   var data = {
     from: 'Excited User <me@samples.mailgun.org>',
     // to: '18084998616@messaging.sprintpcs.com',
-    //vzwpix.com -- verizon
     to: '8083543054@mms.att.net', //att
     // to: '8087814110@vzwpix.com',
     // to: '18083873284@tmomail.net',
