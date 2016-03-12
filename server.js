d9620e7a273ce96a63d7ff9278b33aebae368a4c
benjaminpettus <benjaminpettus@gmail.com>
2016-03-11 23:30:14 -1000

    sending mms message to att only

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
 // var jade = require('jade');
-
+var mailgun = require('mailgun-js')({apiKey: 'key-5cf681c09c48516a8e0b8b00a580969c', domain: 'sandbox08ea930e43c140d699bd4406076fd445.mailgun.org'});
+var filepath = path.join(__dirname, 'IMG_1534.JPG');
 
 var app = express();
 
@@ -28,6 +29,28 @@ app.post('/admin', function (req, res) {
   res.render('index');
 });
 
+app.post('/message', function (req, res) {
+  var data = {
+    from: 'Excited User <me@samples.mailgun.org>',
+    // to: '18084998616@messaging.sprintpcs.com',
+    //vzwpix.com -- verizon
+    to: '8083543054@mms.att.net', //att
+    // to: '8087814110@vzwpix.com',
+    // to: '18083873284@tmomail.net',
+    // to: '8087814110@vtext.com',
+    subject: 'Hello',
+    text: 'Someone is at your door',
+    attachment: filepath
+
+  };
+  mailgun.messages().send(data, function (error, body) {
+    if(error) {
+      console.log(error);
+    } else {
+      console.log(body);
+    }
+  });
+});
 
 
 
